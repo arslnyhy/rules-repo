17a5f639ad48e7aeb21f3ee6755261f8689a3ef7
admin@admin.com <admin@admin.com>
2025-02-14 09:06:16 +0000

    new policy test

---
+++ test/default.py
@@ -0,0 +1,21 @@
+"""
+Here you can explain the purpose of your test file
+
+"""
+import pytest
+from comfy.compliance import *  # noqa
+
+# if all tests are for one platform: short-cut specification is like so:
+# pytestmark = pytest.mark.platform('Cisco_IOS')
+
+'''
+# skeleton example of a rule function
+
+@high(
+    name='Arbitrary text describing this rule'
+)
+def rule_function(source: Source):
+    # write here your assertions
+    # assert 'ssh mgmt-auth public-key' in source.lines
+    pass
+'''
